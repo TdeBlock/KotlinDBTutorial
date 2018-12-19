f3369c8a5911cc6916481c13046aea59e7dd8bd6
TdeBlock <Tee_db@Outlook.com>
2018-12-19 16:19:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tom/kotlindbtutorial/ContactData.kt
@@ -0,0 +1,21 @@
+package com.example.tom.kotlindbtutorial
+
+import kotlin.math.ln
+
+class ContactData {
+
+    var conID: Int ?= null
+    var FirstName: String ?= null
+    var LastName: String ?= null
+    var Email: String ?= null
+    var PhoneNumber: String ?= null
+
+    constructor(id: Int, fname: String, lname: String, email: String, pnum: String){
+
+        this.conID = id
+        this.FirstName = fname
+        this.LastName = lname
+        this.Email = email
+        this.PhoneNumber = pnum
+    }
+}
