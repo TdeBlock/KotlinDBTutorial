e9a083137ca05dadf2ed8b7187ae27219860e52c
TdeBlock <Tee_db@Outlook.com>
2018-12-21 10:53:40 +0100

    Updated activities

---
+++ app/src/main/java/com/example/tom/kotlindbtutorial/ContactManager.kt
@@ -0,0 +1,12 @@
+package com.example.tom.kotlindbtutorial
+
+import android.app.Activity
+import android.os.Bundle
+
+class ContactManager : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_manager)
+    }
+}
